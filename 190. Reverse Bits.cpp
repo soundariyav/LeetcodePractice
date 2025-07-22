00f7194069948ae77afe48cdd2cd7d39760250db
Soundariya Vijayakumar <115153779+soundariyav@users.noreply.github.com>
2025-07-21 19:22:38 -0700

    Create 190. Reverse Bits.cpp

---
+++ 190. Reverse Bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        auto rev=0;
+        for (int i = 0; i < 32; i++) {
+        rev <<= 1;
+        rev |= (n & 1);
+        n >>= 1;
+    }
+    return rev;
+    }
+};
